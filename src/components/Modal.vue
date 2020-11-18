127b7df261e2d83414b616f5edab4488833e34ed
Hugo Rodrigues Gomes da Silva <hugo.ifce@gmail.com>
2020-11-18 20:48:38 -0300

    Create modal component

---
+++ src/components/Modal.vue
@@ -0,0 +1,84 @@
+<template>
+  <div name="modal" v-if="showModal">
+    <div class="modal-backdrop-container">
+      <div class="modal-backdrop">
+        <div class="modal-container">
+          <div class="modal-header">
+            <span class="header">
+              <slot name="header"></slot>
+            </span>
+
+            <span class="close-button" @click="showModal = false">
+              <img src="../assets/x-mark.svg" alt="Fechar" height="20px" />
+            </span>
+          </div>
+
+          <div class="modal-body">
+            <slot name="body"> </slot>
+          </div>
+
+          <div class="modal-footer">
+            <slot name="footer"> </slot>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { Component, Vue } from 'vue-property-decorator'
+
+@Component({
+  components: {},
+  props: ['showModal'],
+  data () {
+    return {
+      showModal: false
+    }
+  }
+})
+export default class Modal extends Vue {}
+</script>
+
+<style lang="less">
+.modal-backdrop-container {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.5);
+  display: table;
+  transition: opacity 0.3s ease;
+  .modal-backdrop {
+    width: 100%;
+    margin: auto;
+    margin-top: 5vh;
+
+    .modal-container {
+      margin: 0px auto;
+      background-color: white;
+      width: 500px;
+      height: fit-content;
+
+      .modal-header {
+        margin: 0px;
+        padding: 16px;
+        font-size: 1.5em;
+        font-weight: 600;
+        position: relative;
+        .close-button {
+          position: absolute;
+          right: 16px;
+          cursor: pointer;
+        }
+      }
+
+      .modal-body {
+        padding: 40px;
+      }
+    }
+  }
+}
+</style>
