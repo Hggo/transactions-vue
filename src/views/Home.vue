f9d3f8ea7690c35afad26ec7f013a379a8dbb88c
Hugo Rodrigues Gomes da Silva <hugo.ifce@gmail.com>
2020-11-22 20:16:08 -0300

    Add transaction listing into home component

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -7,10 +7,16 @@
 <script lang="ts">
 import vue from 'vue'
 import TransactionTable from '@/components/TransactionTable.vue'
+import { listTransactions } from '@/services/transaction.service'
 
 export default vue.extend({
   components: {
     TransactionTable
+  },
+  created () {
+    listTransactions().then((trasactions) => {
+      this.$store.commit('setTransactions', trasactions.data)
+    })
   }
 })
 </script>
